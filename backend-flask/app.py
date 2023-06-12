cdcdc594dd9157cda4106e631449f4259dace188
Sameed Uddin <msun_007_1@yahoo.com>
2023-06-12 18:52:40 +0000

    added x-ray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
-
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
@@ -23,11 +22,21 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
+# X-Ray --------------
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+
 # HoneyComb --------------
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+# X-RAY ------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 # Show this in the logs within the backend-flask app (STDOUT)
 
