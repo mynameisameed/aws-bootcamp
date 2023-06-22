64d6f8a45ab87418a50bd82790870a24d2b8a083
Mohammed Sameed Uddin <msun_007_1@yahoo.com>
2023-06-22 15:17:34 +0000

    adding codespaces requirements

---
+++     .devcontainer/postCreateCommand.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# awscli
+cd /workspacesaws-bootcamp-cruddur-2023/ && wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install -y
+
+# npm install  frontend
+cd /workspaces/aws-bootcamp-cruddur-2023/frontend-react-js && npm update -g && npm i;
+
+# backend pip requirements
+cd /workspaces/aws-bootcamp-cruddur-2023/backend-flask && pip3 install -r requirements.txt;
+
+# Postgresql
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list';
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -;
+sudo apt update -y
+sudo apt install -y postgresql-client-13 libpq-dev
