48beb8df7012bd162c33c1790ab7503ae691bd8f
Sameed Uddin <msun_007_1@yahoo.com>
2023-06-22 13:19:20 +0000

    minor changes

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -80,8 +80,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 # CloudWatch -------------
@@ -98,14 +98,14 @@ with app.app_context():
   def init_rollbar():
       """init rollbar module"""
       rollbar.init(
-          # access token
-          '75c5706913864f498bba4a811147c00d',
-          # environment name
-          'production',
-          # server root directory, makes tracebacks prettier
-          root=os.path.dirname(os.path.realpath(__file__)),
-          # flask already sets up logging
-          allow_logging_basic_config=False)
+        # access token
+        '75c5706913864f498bba4a811147c00d',
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
 
       # send exceptions from `app` to rollbar, using flask's signal system.
       got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
