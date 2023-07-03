4a7b9ad48c73ffe26a18b6c9ad937403c3c564ec
Sameed Uddin <msun_007_1@yahoo.com>
2023-07-03 18:56:31 +0000

    added custom UI themes

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -159,21 +159,21 @@ def data_create_message():
   return
 
 @app.route("/api/activities/home", methods=['GET'])
-@xray_recorder.capture('activites_home')
+@xray_recorder.capture('activities_home')
 def data_home():
   access_token = extract_access_token(request.headers)
   try:
-    claims =  cognito_jwt_token.verify(access_token)
-    #authemticated request
-    app.logger.debug('authenticated')
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    app.logger.debug("authenicated")
     app.logger.debug(claims)
     app.logger.debug(claims['username'])
     data = HomeActivities.run(cognito_user_id=claims['username'])
   except TokenVerifyError as e:
-    #unathenticated request
-    app.logger.debug("unauthenticated")
+    # unauthenicatied request
+    app.logger.debug(e)
+    app.logger.debug("unauthenicated")
     data = HomeActivities.run()
-
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
